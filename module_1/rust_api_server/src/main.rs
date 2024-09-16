97eabff16a2e3de8365cab316b77e28f6b01976a
Rashid Rasul <rstrategist@users.noreply.github.com>
2024-09-16 10:41:29 +0000

    Added Rust API server

---
+++ module_1/rust_api_server/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/").to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+}
