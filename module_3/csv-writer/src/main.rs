148da0fecef9ac825e9a0aea7cb461319bb7a403
rstrategist <rstrategist@users.noreply.github.com>
2024-11-25 20:28:24 +0100

    Update folder names

--- module_3/csv-writer/src/main.rs
+++ module_3/csv-writer/src/main.rs
@@ -1,5 +1,8 @@
 // Example code for reading data from a csv, updating values and writing to a new csv
 
+// Rust CSV Cookbook: https://docs.rs/csv/1.0.0/csv/cookbook/index.html
+// Cookbook Github (see examples folder): https://github.com/BurntSushi/rust-csv
+
 // 1. Reading the CSV File: We open products.csv in read mode and create a Reader to parse the CSV file.
 // 2. Processing Each Record: We iterate through each record, extract the product name and price, calculate a 10% discount, and store the results in a vector.
 // 3. Writing to a New CSV File: We open discounted_products.csv in write mode, write the header row, and then write each product’s name and discounted price to the new file.
